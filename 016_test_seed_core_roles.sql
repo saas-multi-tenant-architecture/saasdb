ac31ac881dc5dbf37bd06ff0ea03c280f08221df
JLP <football@pitney.com>
2025-12-27 23:43:55 -0500

    Removed Tests in prep for pgTap Implemetation

--- 016_test_seed_core_roles.sql
+++
@@ -1,9 +0,0 @@
--- 016_test_seed_core_roles.sql
--- Purpose: Seed default core roles for development/testing
-
-INSERT INTO core.roles (name, priority, description)
-VALUES
-  ('admin', 1, 'Organization administrator'),
-  ('manager', 2, 'Unit manager'),
-  ('user', 3, 'Regular user')
-ON CONFLICT (name) DO NOTHING;
